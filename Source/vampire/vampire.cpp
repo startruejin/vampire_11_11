79cb0350f0807b5c17383c679a399922b9a01434
startruejin <101182657+startruejin@users.noreply.github.com>
2024-11-11 20:20:15 +0900

    main1

---
+++ Source/vampire/vampire.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "vampire.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, vampire, "vampire" );
+
+DEFINE_LOG_CATEGORY(Logvampire)
+ 
---
+++ Source/vampire/vampireGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "vampireGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AvampireGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AvampireGameMode();
+};
+
+
+
